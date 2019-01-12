ecabe874bac99e2589de577735d9cd5f42ea7dab
Ginger Lee Kretschmer <ginger.kretschmer@instrument.com>
2019-01-12 13:46:06 -0800

    Update announce styling

--- client/src/announce/Announce.js
+++ client/src/announce/Announce.js
@@ -38,19 +38,17 @@ class Announce extends Component {
   render() {
     console.log(this.state.response);
     return (
-      <div className="Announce">
-        <header className="Announce-header">
-          <h1>Announcements</h1>
+      <div className="announce">
+        <header className="announce-header">
+          <h1>📣 Growing Gardens Announcements 📣</h1>
         </header>
         <p>{this.state.response}</p>
         <form onSubmit={this.handleSubmit}>
-          <p>
-            <strong>Send Announcement:</strong>
-          </p>
-          <textarea
+          <p className="announce-subheader">Draft push notification below:</p>
+          <textarea className="announce-textarea"
             onChange={e => this.setState({ message: e.target.value })}
           />
-          <button className="submit-button" type="submit">Submit</button>
+          <button className="submit-button" type="submit">Publish</button>
         </form>
       </div>
     );
