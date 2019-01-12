26d90ff0bd1c2466f94dd9efd516bf7ee2e71eb7
Zack Manning <zack.manning@gmail.com>
2019-01-12 14:09:08 -0800

    push notifications for topic

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,17 +1,51 @@
 import React, { Component } from 'react';
-import './App.css';
+import firebase from 'firebase';
 
 class Home extends Component {
 
   componentDidMount() {}
 
-  render() {
-    return (
-        <div className="Home">
-            {/* TODO put what goes on the home page here */}
-        </div>
-    );
-  }
+    /**
+     * Initialize firebase
+     */
+    setupFirebase() {
+        var config = {
+            apiKey: "AAAAwDyW1Bo:APA91bHQbOnDKK9GTX0xd8-3fiEmZXiiWGib6-uh_sfasgIZ7J3vRn6QoaDiMvGT40eRUQydmtJpbOT2dCjjej6cQX42oovf4yYfWOkUdHgnlBiqxw4h8tA5sNI1YW8Qj8ZrVB_ailn1\n",
+            authDomain: "seedsonality.firebaseapp.com",
+            databaseURL: "https://seedsonality.firebaseio.com",
+            projectId: "seedsonality",
+            storageBucket: "seedsonality.appspot.com",
+            messagingSenderId: "825650238490",
+        };
+        firebase.initializeApp(config);
+
+        const messaging = firebase.messaging();
+        messaging.usePublicVapidKey("BGq8Hpjffnqs-gCsFRTzzpaN-8Hohyrp5bWi5F5wOTHn0V8pN3WFd-Dn-IEKxsAjs6XSYiAvxGQ8RYUKqklbKlA");
+        messaging.requestPermission().then(function() {
+            console.log('Notification permission granted.');
+            const token = messaging.getToken();
+
+            console.log('dat token', token);
+        }).catch(function(err) {
+            console.log('Unable to get permission to notify.', err);
+        });
+
+        messaging.onMessage(function(payload) {
+            console.log('Message received. ', payload);
+
+            alert('whats up yo')
+        });
+    }
+
+
+    render() {
+        this.setupFirebase();
+        return (
+            <div className="Home">
+                {/* TODO put what goes on the home page here */}
+            </div>
+        );
+    }
 }
 
 export default Home;
--- client/src/announce/Announce.js
+++ client/src/announce/Announce.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './Announce.css';
-import firebase from 'firebase';
 
 class Announce extends Component {
     state = {
@@ -36,44 +35,7 @@ class Announce extends Component {
         this.setState({ response: body, message: '' });
     };
 
-    /**
-     * Initialize firebase
-     */
-    setupFirebase() {
-        var config = {
-            apiKey: "AAAAwDyW1Bo:APA91bHQbOnDKK9GTX0xd8-3fiEmZXiiWGib6-uh_sfasgIZ7J3vRn6QoaDiMvGT40eRUQydmtJpbOT2dCjjej6cQX42oovf4yYfWOkUdHgnlBiqxw4h8tA5sNI1YW8Qj8ZrVB_ailn1\n",
-            authDomain: "seedsonality.firebaseapp.com",
-            databaseURL: "https://seedsonality.firebaseio.com",
-            projectId: "seedsonality",
-            storageBucket: "seedsonality.appspot.com",
-            messagingSenderId: "825650238490",
-        };
-        firebase.initializeApp(config);
-
-        const messaging = firebase.messaging();
-        messaging.usePublicVapidKey("BGq8Hpjffnqs-gCsFRTzzpaN-8Hohyrp5bWi5F5wOTHn0V8pN3WFd-Dn-IEKxsAjs6XSYiAvxGQ8RYUKqklbKlA");
-        messaging.requestPermission().then(function() {
-            console.log('Notification permission granted.');
-            // TODO(developer): Retrieve an Instance ID token for use with FCM.
-            const token = messaging.getToken();
-            console.log('dat token', token);
-        }).catch(function(err) {
-            console.log('Unable to get permission to notify.', err);
-        });
-
-// Handle incoming messages. Called when:
-// - a message is received while the app has focus
-// - the user clicks on an app notification created by a service worker
-//   `messaging.setBackgroundMessageHandler` handler.
-        messaging.onMessage(function(payload) {
-            console.log('Message received. ', payload);
-
-            alert('whats up yo')
-        });
-    }
-
     render() {
-        this.setupFirebase();
         return (
             <div className="Announce">
                 <header className="Announce-header">
